67e016fc2e87843313cf059af3b26d4415f895e9
realstoman <realstoman@gmail.com>
2023-02-15 17:44:25 +0430

    Form light and dark mode and countries list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { FiGithub, FiTwitter } from 'react-icons/fi';
+import Button from './components/Button';
+import { countries } from './data/countries';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -13,27 +16,29 @@ function App() {
 						</h3>
 					</a>
 				</div>
-				<div className="w-full px-6 py-4 mt-6  bg-white shadow-md rounded-md sm:rounded-lg max-w-sm">
+				<div className="w-full px-6 py-4 mt-6  bg-white dark:bg-gray-900 shadow-md rounded-md sm:rounded-lg max-w-sm">
 					<form>
 						<div>
 							<label
-								htmlFor="name"
-								className="block text-sm font-medium text-gray-700 undefined"
+								htmlFor="fullName"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
-								Name
+								Full Name
 							</label>
 							<div className="flex flex-col items-start">
 								<input
 									type="text"
-									name="name"
-									className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+									name="fullName"
+									placeholder="Full Name"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
 								/>
 							</div>
 						</div>
 						<div className="mt-4">
 							<label
 								htmlFor="email"
-								className="block text-sm font-medium text-gray-700 undefined"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
 								Email
 							</label>
@@ -41,14 +46,38 @@ function App() {
 								<input
 									type="email"
 									name="email"
-									className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+									placeholder="Email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
 								/>
 							</div>
 						</div>
 						<div className="mt-4">
 							<label
+								for="country"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>
+								Select your country
+							</label>
+							<select
+								id="country"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							>
+								{countries.map((country) => (
+									<option
+										key={country.code}
+										value={country.code}
+									>
+										{country.name}
+									</option>
+								))}
+							</select>
+						</div>
+
+						<div className="mt-4">
+							<label
 								htmlFor="password"
-								className="block text-sm font-medium text-gray-700 undefined"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
 								Password
 							</label>
@@ -56,14 +85,16 @@ function App() {
 								<input
 									type="password"
 									name="password"
-									className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+									placeholder="Password"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
 								/>
 							</div>
 						</div>
 						<div className="mt-4">
 							<label
 								htmlFor="password_confirmation"
-								className="block text-sm font-medium text-gray-700 undefined"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
 								Confirm Password
 							</label>
@@ -71,13 +102,15 @@ function App() {
 								<input
 									type="password"
 									name="password_confirmation"
-									className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+									placeholder="Confirm password"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
 								/>
 							</div>
 						</div>
 						<a
 							href="#"
-							className="text-xs text-purple-600 hover:underline"
+							className="text-xs text-purple-600 hover:text-purple-800 dark:text-purple-300 dark:hover:text-purple-100 hover:underline pt-1"
 						>
 							Forget Password?
 						</a>
@@ -87,14 +120,14 @@ function App() {
 							</button>
 						</div>
 					</form>
-					<div className="mt-4 text-grey-600">
+					<div className="mt-4 text-zinc-600 text-md dark:text-zinc-300">
 						Already have an account?{' '}
 						<span>
 							<a
-								className="text-purple-600 hover:underline"
+								className="text-purple-600 hover:text-purple-800 dark:text-purple-400 dark:hover:text-purple-100 hover:underline"
 								href="#"
 							>
-								Log in
+								Login instead
 							</a>
 						</span>
 					</div>
@@ -104,34 +137,16 @@ function App() {
 						<hr className="w-full" />
 					</div>
 					<div className="my-6 space-y-2">
-						<button
-							aria-label="Login with Google"
-							type="button"
-							className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 32 32"
-								className="w-5 h-5 fill-current"
-							>
-								<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-							</svg>
-							<p>Login with Google</p>
-						</button>
-						<button
-							aria-label="Login with GitHub"
-							role="button"
-							className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 32 32"
-								className="w-5 h-5 fill-current"
-							>
-								<path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-							</svg>
-							<p>Login with GitHub</p>
-						</button>
+						<Button
+							title="Continue with Github"
+							ariaLabel="Continue with Github"
+							icon={<FiGithub className="text-xl" />}
+						/>
+						<Button
+							title="Continue with Twitter"
+							ariaLabel="Continue with Twitter"
+							icon={<FiTwitter className="text-xl" />}
+						/>
 					</div>
 				</div>
 			</div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ title, icon, ariaLabel }) => {
+	return (
+		<button
+			aria-label={ariaLabel}
+			type="button"
+			className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 flex items-center justify-center w-full py-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-200 dark:border-gray-600 hover:border-purple-400 focus:ring-purple-400 dark:hover:border-purple-600"
+		>
+			{icon}
+			<p>{title}</p>
+		</button>
+	);
+};
+
+export default Button;
