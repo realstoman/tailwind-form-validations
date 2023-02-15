e6b95bf0ce5073805c042c975d5be2a84bbaf264
realstoman <realstoman@gmail.com>
2023-02-15 18:53:22 +0430

    Add form validations

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,33 @@ import Button from './components/Button';
 import { countries } from './data/countries';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [data, setData] = useState({
+		fullName: '',
+		email: '',
+		country: '',
+		password: '',
+	});
+
+	const handleRegistration = (e) => {
+		e.preventDefault();
+
+		console.log(data);
+	};
+
+	const { ...allData } = data;
+
+	// Disable submit button until all fields are filled in
+	const canSubmit = [...Object.values(allData)].every(Boolean);
 
 	return (
 		<div className="flex justify-center items-center min-h-screen px-4">
-			<div className="flex flex-col items-center pt-6 sm:justify-center w-full">
-				<div>
-					<a href="/">
-						<h3 className="text-4xl font-bold text-purple-600">
-							Logo now
-						</h3>
-					</a>
-				</div>
-				<div className="w-full px-6 py-4 mt-6  bg-white dark:bg-gray-900 shadow-md rounded-md sm:rounded-lg max-w-sm">
-					<form>
+			<div className="flex flex-col items-center py-6 sm:justify-center w-full">
+				<div className="w-full px-6 py-6  bg-white dark:bg-gray-900 shadow-md rounded-md sm:rounded-lg max-w-sm">
+					<form
+						action=""
+						onSubmit={handleRegistration}
+						className="group"
+					>
 						<div>
 							<label
 								htmlFor="fullName"
@@ -30,9 +43,19 @@ function App() {
 									type="text"
 									name="fullName"
 									placeholder="Full Name"
-									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500 placeholder-gray-300 valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+									pattern="[0-9a-zA-Z ]{6,}"
 									required
+									onChange={(e) => {
+										setData({
+											...data,
+											fullName: e.target.value,
+										});
+									}}
 								/>
+								<span className="mt-1 hidden text-sm text-red-400">
+									Full name must be at least 6 characters long
+								</span>
 							</div>
 						</div>
 						<div className="mt-4">
@@ -47,21 +70,38 @@ function App() {
 									type="email"
 									name="email"
 									placeholder="Email"
-									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500 placeholder-gray-300 valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+									autoComplete="off"
 									required
+									pattern="[a-z0-9._+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+									onChange={(e) => {
+										setData({
+											...data,
+											email: e.target.value,
+										});
+									}}
 								/>
+								<span className="mt-1 hidden text-sm text-red-400">
+									Please enter a valid email address.{' '}
+								</span>
 							</div>
 						</div>
 						<div className="mt-4">
 							<label
-								for="country"
+								htmlFor="country"
 								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
 								Select your country
 							</label>
 							<select
 								id="country"
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
+								onChange={(e) => {
+									setData({
+										...data,
+										country: e.target.value,
+									});
+								}}
 							>
 								{countries.map((country) => (
 									<option
@@ -86,9 +126,20 @@ function App() {
 									type="password"
 									name="password"
 									placeholder="Password"
-									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500 placeholder-gray-300 valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+									autoComplete="off"
 									required
+									pattern="[0-9a-zA-Z]{8,}"
+									onChange={(e) => {
+										setData({
+											...data,
+											password: e.target.value,
+										});
+									}}
 								/>
+								<span className="mt-1 hidden text-sm text-red-400">
+									Password must be at least 8 characters.{' '}
+								</span>
 							</div>
 						</div>
 						<div className="mt-4">
@@ -103,9 +154,20 @@ function App() {
 									type="password"
 									name="password_confirmation"
 									placeholder="Confirm password"
-									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500 placeholder-gray-300 valid:[&:not(:placeholder-shown)]:border-green-500 [&:not(:placeholder-shown):not(:focus):invalid~span]:block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+									autoComplete="off"
 									required
+									pattern="[0-9a-zA-Z]{8,}"
+									onChange={(e) => {
+										setData({
+											...data,
+											password: e.target.value,
+										});
+									}}
 								/>
+								<span className="mt-1 hidden text-sm text-red-400">
+									Password must be at least 8 characters.{' '}
+								</span>
 							</div>
 						</div>
 						<a
@@ -115,9 +177,21 @@ function App() {
 							Forget Password?
 						</a>
 						<div className="flex items-center mt-4">
-							<button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-								Register
+							<button
+								type="submit"
+								disabled={!canSubmit}
+								className="w-full text-white bg-purple-700 hover:bg-purple-600 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center mt-2 disabled:bg-gradient-to-br disabled:from-gray-100 disabled:to-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed group-invalid:bg-gradient-to-br group-invalid:from-gray-100 group-invalid:to-gray-300 group-invalid:text-gray-400 group-invalid:pointer-events-none group-invalid:opacity-70"
+							>
+								Create account
 							</button>
+
+							{/* <button
+								type="submit"
+								disabled={!canSubmit}
+								className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600 disabled:bg-gradient-to-br disabled:from-gray-100 disabled:to-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed group-invalid:bg-gradient-to-br group-invalid:from-gray-100 group-invalid:to-gray-300 group-invalid:text-gray-400 group-invalid:pointer-events-none group-invalid:opacity-70"
+							>
+								Register
+							</button> */}
 						</div>
 					</form>
 					<div className="mt-4 text-zinc-600 text-md dark:text-zinc-300">
