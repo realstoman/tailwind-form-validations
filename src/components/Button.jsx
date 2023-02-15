5f5b88f59cf09a464e3fcebc92b1374cd0216a6e
realstoman <realstoman@gmail.com>
2023-02-15 19:00:50 +0430

    Minor design updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ title, icon, ariaLabel }) => {
 		<button
 			aria-label={ariaLabel}
 			type="button"
-			className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 flex items-center justify-center w-full py-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-200 dark:border-gray-600 hover:border-purple-400 focus:ring-purple-400 dark:hover:border-purple-600"
+			className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 flex items-center justify-center w-full py-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-300 dark:border-gray-700 hover:border-purple-400 focus:ring-purple-400 dark:hover:border-purple-600"
 		>
 			{icon}
 			<p>{title}</p>
